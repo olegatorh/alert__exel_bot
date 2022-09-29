5bcc57af429bfb42e3a2d51bddaa7f7404d440eb
olegatorh <60426669+olegatorh@users.noreply.github.com>
2022-09-29 22:30:19 +0000

    Update get_file.py

--- get_file.py
+++ get_file.py
@@ -10,9 +10,3 @@ def get_file(url):
     with open("Daily_checklist.xlsx", "wb") as f:
         f.write(response.content)
 
-
-# get_file(
-#      'https://pragmaticplay-my.sharepoint.com/personal/dmitriy_makashev_pragmaticplay_com/_layouts/15/download.aspx?e=HIIozD&share=ES7fPkUVQ-BNrlLYnC8jF8wBfWMsFPEPgiRkjVsoqHhufQ')
-#
-
-print()
